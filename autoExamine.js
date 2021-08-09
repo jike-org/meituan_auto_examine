8c364e51e0313a28b9609a01778cba5660df52d8
wangtengfei <776095293@qq.com>
2021-08-09 16:23:50 +0800

    完善代码

--- autoExamine.js
+++ autoExamine.js
@@ -1,126 +1,16 @@
-//import $ from "jquery";
 /**
- * datetime: 2021-01-07 10:00:00
- * author: wangtengfei
+ * create_time: 2021-01-07 10:00:00
+ * update_time: 2021-08-09 16:19:00
+ * author: WangTengfei
+ * email: 776095293@qq.com
  * 美团外卖商家后台，自动审核订单脚本
  */
 
-//从cookie中获取指定值
-function getCookie(key) {
-    return document.cookie.length > 0 && (c_start = document.cookie.indexOf(key + "="),
-    -1 != c_start) ? (c_start = c_start + key.length + 1,
-    c_end = document.cookie.indexOf(";", c_start),
-    -1 == c_end && (c_end = document.cookie.length),
-    unescape(document.cookie.substring(c_start, c_end))) : ""
-}
+const http = axios.create({
+    baseURL: 'https://shangoue.meituan.com/v1/prescription',
+    timeout: 5 * 1000
+})
 
-//具体请求的方法
-var request = {
-    //定时任务timerId
-    timerId:0,
-    //商家id:从cookie中拿到acctid
-    acctId: getCookie("acctId") ? getCookie("acctId") : 73164926,
-    //查询有没有订单过来
-    query: function() {
-        var that = this;
-        let url = "https://shangoue.meituan.com/v1/prescription/unaudited/query?startTime=&endTime=&pageNum=1&pageSize=20"
-        //发起请求
-        let response = $.get(url, {}, function(data, status, xhr) {
-            if (xhr.status != 200) {
-                that.writeLog("", "网络请求失败：" + xhr.status);
-                return false;
-            }
-            if (!data || data.code != 0) {
-                let logMsg = data.msg;
-                if (data.code != 1001) {
-                    logMsg = "查询订单失败：" + data.msg;
-                }
-                that.writeLog("", logMsg);
-                //清除定时timerId
-                window.clearInterval(that.timerId);
-                return false;
-            }
-            let pageData = data.data.pageData;
-            if (pageData.commonPageInfo.totalCount <= 0) {
-                that.writeLog("", "订单列表为空：" + pageData.commonPageInfo.totalCount);
-                return false;
-            }
-            if (pageData.pageList.length <= 0) {
-                that.writeLog("", "pageList数组为空：" + pageData.pageList.length);
-                return false;
-            }
-            let orderList = pageData.pageList;
-            //循环处理每个订单
-            for (var i = 0; i < orderList.length; i++) {
-                that.detail(that, orderList[i]);
-            }
-        });
-    },
-    //查询订单详情,提交审核需要某些参数
-    detail: function(that, order) {
-        let url = "https://shangoue.meituan.com/v1/prescription/order/detail/query?auditViewId=" + order.auditViewId + "&acctId=" + this.acctId + "&wmPoiId=" + order.poiId;
-        let response = $.get(url, {}, function(data, status, xhr) {
-            if (xhr.status != 200) {
-                that.writeLog(order.auditViewId, "查询订单详情,网络请求失败：" + xhr.status)
-                return false;
-            }
-            if (!data || data.code != 0) {
-                that.writeLog(order.auditViewId, "查询订单详情失败：" + data.msg);
-                return false;
-            }
-            if (!data.data) {
-                that.writeLog(order.auditViewId, "订单详情为空");
-                return false;
-            }
-            //审核订单
-            that.commit(that, order.poiId, order.auditViewId, order.auditStatus, data.data.auditReason, null);
-        });
-    },
-    //提交审核 POST
-    commit: function(that, wmPoiId, auditViewId, auditStatus, auditReason, auditReasonType) {
-        let url = "https://shangoue.meituan.com/v1/prescription/order/audit/update"
-        let param = {
-            wmPoiId: wmPoiId,
-            auditViewId: auditViewId,
-            auditStatus: 2,//auditStatus,
-            auditReason: auditReason,
-            auditReasonType: auditReasonType
-        }
-        let response = $.post(url, param, function(data, status, xhr) {
-            if (xhr.status != 200) {
-                that.writeLog(auditViewId, "订单审核请求失败：" + xhr.status)
-                return false;
-            }
-            if (data.code != 0) {
-                that.writeLog(auditViewId, "订单审核失败：" + data.msg)
-                return false;
-            }
-            that.writeLog(auditViewId, "订单审核成功：" + data.msg)
-            return true;
-        });
-    },
-    //打印日志
-    writeLog: function(orderId, msg) {
-        var orderInfo = "";
-        if (orderId) {
-            orderInfo = "订单id：" + orderId;
-        }
-        console.log(orderInfo + msg);
-    }
-}
-
-//定时执行
-request.timerId = window.setInterval(function() {
-    var datetime = new Date();
-    timeStr = "任务运行时间：";
-    timeStr += datetime.getFullYear() + "-" + datetime.getMonth() + 1 + "-" + datetime.getDate() + " " + datetime.getHours() + ":" + datetime.getMinutes() + ":" + datetime.getSeconds();
-    request.writeLog("", timeStr);
-    request.query();
-}, 5 * 1000);
-request.writeLog("", "定时器id:" + request.timerId);
-
-// import VueCookies from 'vue-cookies'
-// Vue.use(VueCookies)
 // 测试，方便修改为vue的方式
 const vue = new Vue({
     data () {
@@ -128,7 +18,7 @@ const vue = new Vue({
             /**
              * NodeJS.Timer
              */
-            timer_id: 0,
+            timerId: 0,
             //商家id:从cookie中拿到acctid
             acctId: this.$cookies.isKey('acctId') ? this.$cookies.get('acctId') : 73164926,
             time_interval: 5 * 1000 // 5s
@@ -139,52 +29,102 @@ const vue = new Vue({
     },
     methods: {
         run () {
-            this.log(0, this.$cookies.get('acctId'))
             // 注册一个定时器
             this.timerId = setInterval(() => {
-                // this.query()
+                this.query()
             }, this.time_interval)
         },
         //查询有没有订单过来
         query () {
-            const url = "https://shangoue.meituan.com/v1/prescription/unaudited/query?startTime=&endTime=&pageNum=1&pageSize=20"
-            //发起请求
-            const response = $.get(url, {}, function(data, status, xhr) {
-                if (xhr.status != 200) {
-                    this.log(0, "网络请求失败：" + xhr.status);
-                    return false;
+            const url = "/unaudited/query?startTime=&endTime=&pageNum=1&pageSize=20"
+            http.get(url).then(response => {
+                if (response.status !== 200) {
+                    throw new Error(response.statusText)
                 }
-                if (!data || data.code != 0) {
-                    let logMsg = data.msg;
-                    if (data.code != 1001) {
-                        logMsg = "查询订单失败：" + data.msg;
+                const data = response.data
+                if (data.code !== 0) {
+                    let logMsg = data.msg
+                    if (data.code !== 1001) {
+                        logMsg = "查询订单失败：" + data.msg
                     }
-                    this.log(0, logMsg);
                     //清除定时timerId
                     window.clearInterval(this.timerId);
-                    return false;
+                    throw new Error(logMsg)
                 }
-                let pageData = data.data.pageData;
+                const pageData = data.data.pageData
                 if (pageData.commonPageInfo.totalCount <= 0) {
-                    this.log(0, "订单列表为空：" + pageData.commonPageInfo.totalCount);
-                    return false;
+                    throw new Error(`订单列表为空：${pageData.commonPageInfo.totalCount}`);
                 }
                 if (pageData.pageList.length <= 0) {
-                    this.log(0, "pageList数组为空：" + pageData.pageList.length);
-                    return false;
+                    throw new Error(`pageList数组为空：${pageData.pageList.length}`);
                 }
-                let orderList = pageData.pageList;
+                const orderList = pageData.pageList;
                 //循环处理每个订单
-                for (var i = 0; i < orderList.length; i++) {
-                    this.detail(this, orderList[i]);
+                for (let i = 0; i < orderList.length; i++) {
+                    this.detail(orderList[i]);
                 }
-            });
+            }).catch(error => {
+                this.log(0, error.message)
+            })
+        },
+        //查询订单详情,提交审核需要某些参数
+        detail (order) {
+            const url = "/order/detail/query"
+            http.get(url, {
+                params: {
+                    auditViewId: order.auditViewId,
+                    acctId: this.acctId,
+                    wmPoiId: order.poiId
+                }
+            }).then(response => {
+                if (response.status !== 200) {
+                    this.log(order.auditViewId, `查询订单详情,网络请求失败：${response.status}`)
+                    return false
+                }
+                const data = response.data
+                if (data.code !== 0) {
+                    this.log(order.auditViewId, `查询订单详情失败：${data.msg}`)
+                    return false
+                }
+                if (!data.data) {
+                    this.log(order.auditViewId, "订单详情为空")
+                    return false
+                }
+                //审核订单
+                this.commit(order.poiId, order.auditViewId, order.auditStatus, data.data.auditReason, undefined);
+            }).catch(error => {
+                this.log(0, error.message)
+            })
+        },
+        //提交审核 POST
+        commit (wmPoiId, auditViewId, auditStatus, auditReason, auditReasonType) {
+            const url = "/order/audit/update"
+            const params = new FormData()
+            params.append('wmPoiId', wmPoiId)
+            params.append('auditViewId', auditViewId)
+            params.append('auditStatus', 2)
+            params.append('auditReason', auditReason)
+            params.append('auditReasonType', auditReasonType)
+            http.post(url, params).then(response => {
+                if (response.status !== 200) {
+                    this.log(auditViewId, `查询订单详情,网络请求失败：${response.status}`)
+                    return false
+                }
+                const data = response.data
+                if (data.code !== 0) {
+                    this.log(auditViewId, `订单审核失败：${data.msg}`)
+                    return false
+                }
+                this.log(auditViewId, `订单审核成功：${data.msg}`)
+            }).catch(error => {
+                this.log(0, `审核请求失败：${error.message}`)
+            })
         },
         // 格式化日志
         log (orderId, msg = "") {
-            var orderInfo = new String();
+            let orderInfo = ""
             if (orderId) {
-                orderInfo = "订单id：" + orderId;
+                orderInfo = "订单id：" + orderId
             }
             msg = orderInfo + msg
             console.log('自动审单', this.format(), msg)
